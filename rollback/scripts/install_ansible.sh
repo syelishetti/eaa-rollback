843ac0228a60e7d8f0bbd4371401a066aebf00f6
Ubuntu <ubuntu@ip-172-31-26-44.us-west-1.compute.internal>
2019-06-27 23:00:51 +0000

    First commit

---
+++ rollback/scripts/install_ansible.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+datetime=`date +"%m%d%y%s"`
+ansible_install_log=/var/log/wapp/ansible_install_log-${datetime}.log
+
+export PGPASSWORD=dbpassword
+export psql_connect="psql -U dbuser -h localhost -d eaa_upgrade"
+
+dpop_bastion_ip=(`$psql_connect -t -A -c "select bastion_ip from dpop_info"`)
+dpop_bastion_port=3333
+
+echo > $ansible_install_log
+exec > >(tee -a $ansible_install_log) 2>&1
+
+function install_ansible {
+#is_installed_status=`ssh $ip -p $dpop_bastion_port ansible --version; echo $?` > /dev/null
+is_installed_status=`ssh $ip -p $dpop_bastion_port sudo dpkg -l |grep ansible |awk '{print $1}'` > /dev/null
+if [ $is_installed_status != 'ii' ]
+then
+    echo "Installing Ansible dependencies"
+    ssh $ip -p $dpop_bastion_port "sudo apt update; sudo DEBIAN_FRONTEND=noninteractive apt install -y ansible"
+else
+    echo "Ansible has already been installed on the bastion $ip"
+fi
+}
+
+echo "Checking & Installing Ansible on all Bastion Nodes"
+
+for ip in ${dpop_bastion_ip[@]};
+do
+    echo -e "\n################"
+    echo "Working on the bastion ip: $ip"
+    install_ansible
+done
+
+echo -e "\nCompleted Ansible installation and verificaton on Bastion Nodes."
+
---
+++ rollback/scripts/rollback.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+dpop=$1
+proxy=$2
+release=$3
+
+datetime=`date +"%m%d%y%H%M"`
+rollback_log=/var/log/wapp/rollback/rollback_log-$dpop-${datetime}.log
+
+export PGPASSWORD=dbpassword
+export psql_connect="psql -U dbuser -h localhost -d eaa_upgrade"
+dpop_bastion_ip=`$psql_connect -t -A -c  "select bastion_ip from dpop_info where dpop_name='$dpop'"`
+dpop_bastion_port=`$psql_connect -t -A -c  "select port_no from dpop_info where dpop_name='$dpop'"`
+current_version=`$psql_connect -t -A -c  "select current_version from dpop_release_info where release_name='$release' and trash=false"`
+previous_version=`$psql_connect -t -A -c  "select previous_version from dpop_release_info where release_name='$release' and trash=false"`
+#nginx_version=`$psql_connect -t -A -c  "select nginx_version from dpop_nginx_version where rollback_version= 't'"`
+
+rollback_path=/opt/wapp/eaa/rollback/
+local_ansible_path=/opt/wapp/rollback/ansible/
+ansible_path=/opt/wapp/eaa/rollback/ansible/
+inventory_path=$ansible_path/inventory
+
+echo > $rollback_log
+exec > >(tee -a $rollback_log) 2>&1
+
+echo -e "\n###########################"
+echo "Rolling back $proxy on the $dpop via Bastion $dpop_bastion_ip"
+
+echo -e "\n\n###########################"
+echo "Providing ubuntu user permission to the $rollback_path"
+ssh ${dpop_bastion_ip} -p ${dpop_bastion_port} "sudo mkdir -p $rollback_path; sudo chown -R ubuntu:ubuntu $rollback_path"
+
+echo -e "\n\n###########################"
+echo "Copying the local ansible path to the remote Bastion Machine"
+rsync -e 'ssh -p 3333' -arvz --progress --delete $local_ansible_path ubuntu@$dpop_bastion_ip:$ansible_path -v
+
+echo -e "\n\n###########################"
+echo "Running the playbook from Bastion to all the $proxy nodes in $dpop"
+ssh ${dpop_bastion_ip} -p ${dpop_bastion_port} ansible-playbook -i $inventory_path/$dpop -e \"role=$proxy group=$proxy user=ubuntu current_version=$current_version previous_version=$previous_version\" -vv $ansible_path/main.yml
+
+#echo "ssh $dpop_bastion ansible-playbook -i $dpop -e \"group=$proxy user=cfgdamin nginx_version=$nginx_version\" main.yml"
+#ssh ubuntu@${dpop_bastion} "ansible-playbook -i /tmp/$dpop -e \"group=$proxy user=ubuntu nginx_version=xyz\" /opt/scripts/main.yml"
