659515fee91603ac0a37f5ae3552c7884ef0ab75
Ubuntu <ubuntu@ip-172-31-26-44.us-west-1.compute.internal>
2019-06-30 06:24:17 +0000

    Updating changes

--- rollback/scripts/rollback.sh
+++ rollback/scripts/rollback.sh
@@ -35,7 +35,7 @@ rsync -e 'ssh -p 3333' -arvz --progress --delete $local_ansible_path ubuntu@$dpo
 
 echo -e "\n\n###########################"
 echo "Running the playbook from Bastion to all the $proxy nodes in $dpop"
-ssh ${dpop_bastion_ip} -p ${dpop_bastion_port} ansible-playbook -i $inventory_path/$dpop -e \"role=$proxy group=$proxy user=ubuntu current_version=$current_version previous_version=$previous_version rollback=true\" -vv $ansible_path/main.yml
+ssh ${dpop_bastion_ip} -p ${dpop_bastion_port} ansible-playbook -i $inventory_path/$dpop -e \"role=$proxy group=$proxy user=ubuntu current_version=$current_version previous_version=$previous_version rollback=true copy_config=false\" -vv $ansible_path/main.yml
 
 #echo "ssh $dpop_bastion ansible-playbook -i $dpop -e \"group=$proxy user=cfgdamin nginx_version=$nginx_version\" main.yml"
 #ssh ubuntu@${dpop_bastion} "ansible-playbook -i /tmp/$dpop -e \"group=$proxy user=ubuntu nginx_version=xyz\" /opt/scripts/main.yml"
